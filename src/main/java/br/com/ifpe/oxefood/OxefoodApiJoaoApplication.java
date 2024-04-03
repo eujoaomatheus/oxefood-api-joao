e3eafba3495a1235887b3dd8ec099f015d07bf64
aluno <aluno@ifpe>
2024-04-03 18:40:53 -0300

    Funcionando

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiJoaoApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiJoaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OxefoodApiCarlosApplication {
+public class OxefoodApiJoaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OxefoodApiCarlosApplication.class, args);
+		SpringApplication.run(OxefoodApiJoaoApplication.class, args);
 	}
 
 }
