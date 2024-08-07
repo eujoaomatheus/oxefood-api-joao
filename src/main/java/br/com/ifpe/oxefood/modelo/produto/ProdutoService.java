799452170165154f9d0c4bcc98baad2f721e4529
aluno <aluno@ifpe>
2024-08-07 20:06:24 -0300

    updating

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -42,6 +42,36 @@ public Produto obterPorID(Long id) {
 }
 
 
+    public List<Produto> filtrar(String codigo, String titulo, Long idCategoria) {
+
+        List<Produto> listaProdutos = repository.findAll();
+
+        if ((codigo != null && !"".equals(codigo)) &&
+                (titulo == null || "".equals(titulo)) &&
+                (idCategoria == null)) {
+            listaProdutos = repository.consultarPorCodigo(codigo);
+        } else if (
+                (codigo == null || "".equals(codigo)) &&
+                        (titulo != null && !"".equals(titulo)) &&
+                        (idCategoria == null)) {
+            listaProdutos = repository.findByTituloContainingIgnoreCaseOrderByTituloAsc(titulo);
+        } else if (
+                (codigo == null || "".equals(codigo)) &&
+                        (titulo == null || "".equals(titulo)) &&
+                        (idCategoria != null)) {
+            listaProdutos = repository.consultarPorCategoria(idCategoria);
+        } else if (
+                (codigo == null || "".equals(codigo)) &&
+                        (titulo != null && !"".equals(titulo)) &&
+                        (idCategoria != null)) {
+            listaProdutos = repository.consultarPorTituloECategoria(titulo, idCategoria);
+        }
+
+        return listaProdutos;
+    }
+
+
+
     @Transactional
     public void update(Long id, Produto produtoAleterado) {
 
